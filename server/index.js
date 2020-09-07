ae7eee5c628bbc3bff39b74972e143336e542ebd
sunteago <santi.vallejo@gmail.com>
2020-09-07 12:45:12 -0300

    add cors package

--- server/index.js
+++ server/index.js
@@ -1,18 +1,22 @@
-const express = require('express');
-const sequelize = require('./config/db');
+const express = require("express");
+const cors = require("cors");
 
-const todosRoutes = require('./routes/todos');
+const sequelize = require("./config/db");
+
+const todosRoutes = require("./routes/todos");
 
 const PORT = 4000;
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 
-app.use('/todos', todosRoutes);
+app.use("/todos", todosRoutes);
 
 sequelize
   .sync()
   .then(() => app.listen(PORT))
   .then(() => console.log(`Server listening at port ${PORT}`))
-  .catch(err => console.log("An error occured", err))  
+  .catch((err) => console.log("An error occured", err));
